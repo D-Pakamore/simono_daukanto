ad93d872595b79d89e738a10e7dd25cc4eabc69e
D-Pakamore <dpakamore@gmail.com>
2023-10-11 11:24:06 +0300

    update

--- employees/admin.py
+++ employees/admin.py
@@ -3,5 +3,5 @@ from .models import Teacher
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'koefficient']
+    list_display = ['id', 'first_name', 'last_name', 'birth_date', 'phone_number', 'email', 'home_address', 'koefficient']
     exclude = []
--- employees/models.py
+++ employees/models.py
@@ -4,8 +4,12 @@ from koefficient_calculator.models import Koefficient
 class Teacher(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    birth_date = models.CharField(max_length=30, blank=True)
+    phone_number = models.CharField(max_length=30, blank=True)
+    email = models.CharField(max_length=100, blank=True)
+    home_address = models.CharField(max_length=255, blank=True)
     #auto calc
-    koefficient = models.ForeignKey(Koefficient, null=True, on_delete=models.SET_NULL)
+    koefficient = models.ForeignKey(Koefficient, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
